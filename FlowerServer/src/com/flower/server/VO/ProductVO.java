b688c4a26a604b4164a022ece0d254acd41ac607
user <user@user-PC>
2017-06-19 20:03:01 +0900

    VO 동기화 2

---
+++ FlowerServer/src/com/flower/server/VO/ProductVO.java
@@ -0,0 +1,99 @@
+package com.flower.server.VO;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class ProductVO implements Serializable {
+	private String fName;
+	private String imgUrl;
+	private String textUrl;
+	private int fPrice;
+	private int you1;
+	private int you2;
+	private int me1;
+	private int me2;
+	private int color;
+	
+	public ProductVO(String fName, int fPrice, String imgUrl, String textUrl) {
+		this.fName = fName;
+		this.fPrice = fPrice;
+		this.imgUrl = imgUrl;
+		this.textUrl = textUrl;
+	} // constructor
+
+	// setter and getters
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getTextUrl() {
+		return textUrl;
+	}
+
+	public void setTextUrl(String textUrl) {
+		this.textUrl = textUrl;
+	}
+
+	public int getfPrice() {
+		return fPrice;
+	}
+
+	public void setfPrice(int fPrice) {
+		this.fPrice = fPrice;
+	}
+
+	public int getYou1() {
+		return you1;
+	}
+
+	public void setYou1(int you1) {
+		this.you1 = you1;
+	}
+
+	public int getYou2() {
+		return you2;
+	}
+
+	public void setYou2(int you2) {
+		this.you2 = you2;
+	}
+
+	public int getMe1() {
+		return me1;
+	}
+
+	public void setMe1(int me1) {
+		this.me1 = me1;
+	}
+
+	public int getMe2() {
+		return me2;
+	}
+
+	public void setMe2(int me2) {
+		this.me2 = me2;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+	
+	
+	
+}
